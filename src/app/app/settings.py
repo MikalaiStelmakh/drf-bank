32ec45d9da32c5030676bd3969482108175b135d
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2022-09-22 12:40:10 +0200

    Move environment variables to file

--- src/app/app/settings.py
+++ src/app/app/settings.py
@@ -9,12 +9,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1lk3!rnfc#38vc&ppw9n3p$$el^1wpn_oy2hh9507$bk-0*&_1'
+SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-1lk3!rnfc#38vc&ppw9n3p$$el^1wpn_oy2hh9507$bk-0*&_1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'TRUE') == 'TRUE'
 
-ALLOWED_HOSTS = ['*']
+# Should be a single string of hosts with a space between each.
+# For example: 'ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
